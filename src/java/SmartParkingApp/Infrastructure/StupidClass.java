d5648e716133874539b0710b9b4843dcf33c5bfc
Xuân Tùng <xuantung.1998@gmail.com>
2019-08-11 09:42:04 +0700

    OpenCV 4 Integrated

--- src/java/SmartParkingApp/Infrastructure/StupidClass.java
+++ src/java/SmartParkingApp/Infrastructure/StupidClass.java
@@ -1,8 +1,14 @@
 package SmartParkingApp.Infrastructure;
 
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+
 public class StupidClass {
     public static void main(String[] args){
+        System.setProperty("java.library.path", "lib/opencv4/x64");
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+//        System.loadLibrary("opencv_java411");
         Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
         System.out.println("mat = " + mat.dump());
     }
